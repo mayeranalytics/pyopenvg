9a511e3412c77214adf61876b0808037b93bbeff
devan.lai <devan.lai@ee642040-9953-0410-8ed3-659e4658cabf>
2008-08-16 05:55:38 +0000

    Combo setup.py in preparation for merge.

--- setup.py
+++ setup.py
@@ -2,24 +2,43 @@ from distutils.core import setup, Extension
 import Pyrex.Distutils
 import os
 
+libraries = ["libOpenVG"]
+library_dirs = ["."]
+include_dirs = ["include"]
+
 freetype_root = r"C:\Program Files\Freetype"
+FT_libraries = ["freetype"]
+FT_library_dirs = [os.path.join(freetype_root, "lib"),
+                   os.path.join(freetype_root, "bin")]
+FT_include_dirs = ["include",
+                   os.path.join(freetype_root, "include"),
+                   os.path.join(freetype_root, "include", "freetype2")]
 
-libraries = ["freetype"]
-library_dirs = [os.path.join(freetype_root, "lib"),
-                os.path.join(freetype_root, "bin")]
-include_dirs = ["include",
-                os.path.join(freetype_root, "include"),
-                os.path.join(freetype_root, "include", "freetype2")]
+#Pyrex cannot detect the change in included files
+#so it will not normally regenerate the .c
+if os.path.exists(os.path.join("src", "OpenVG", "VG.c")):
+    os.remove(os.path.join("src", "OpenVG", "VG.c"))
 
-setup(name = "PyFreetype",
-      version = "0.0.1",
-      packages = ["FT"],
-      package_dir = {"FT": "src"},
-      py_modules = ["FT.constants"],
-      ext_modules = [Extension("FT.freetype",  ["src/freetype.pyx"],
+setup(name = "PyOpenVG",
+      version = "0.0.2",
+      description = "A Python wrapper for the OpenVG library",
+      license = "BSD",
+      packages = ["OpenVG", "FT"],
+      package_dir = {"OpenVG":"src/OpenVG",
+                     "FT":"src/FT"},
+      py_modules = ["OpenVG.constants", "OpenVG.font", "FT.constants"],
+      ext_modules = [Extension("OpenVG.VG",  ["src/OpenVG/VG.pyx"],
                                libraries=libraries,
                                library_dirs=library_dirs,
-                               include_dirs=include_dirs)],
-      
-      cmdclass = {"build_ext": Pyrex.Distutils.build_ext})
+                               include_dirs=include_dirs),
+                     Extension("OpenVG.VGU", ["src/OpenVG/VGU.pyx"],
+                               libraries=libraries,
+                               library_dirs=library_dirs,
+                               include_dirs=include_dirs),
+                     Extension("FT.freetype", ["src/FT/freetype.pyx"],
+                               libraries=FT_libraries,
+                               library_dirs=FT_library_dirs,
+                               include_dirs=FT_include_dirs)],
+      cmdclass = {"build_ext": Pyrex.Distutils.build_ext}
+)
 
