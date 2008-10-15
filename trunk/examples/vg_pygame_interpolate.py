38c1c7e0eee165b728d503c9edfe840f42ae693a
devan.lai@gmail.com <devan.lai@gmail.com@ee642040-9953-0410-8ed3-659e4658cabf>
2008-10-15 02:03:13 +0000

    Removed obsolete svg module. Added descriptive text to vg_pygame_blend.py example. Switched vg_pygame_interpolate.py, vg_pymunk_moon.py to use new OpenVG.svg module. Removed obsolete vg_pygame_tiger.py example. Added new vg_pygame_svg.py example.

--- trunk/examples/vg_pygame_interpolate.py
+++ trunk/examples/vg_pygame_interpolate.py
@@ -1,27 +1,31 @@
-from __future__ import with_statement
-
 import pygame
 import xml.etree.ElementTree as ET
 
 from OpenVG import VG
 from OpenVG.constants import *
 
-from svg import group_from_element
+from OpenVG.svg import load_svg_element
 
 MORPH_TIME = 1 * 1000
 
-def main():
+def main(width, height):
     pygame.init()
     
     pygame.display.gl_set_attribute(pygame.GL_STENCIL_SIZE, 8)
     pygame.display.gl_set_attribute(pygame.GL_DEPTH_SIZE, 24)
-    srf = pygame.display.set_mode((640,480), pygame.OPENGL | pygame.DOUBLEBUF)
+    srf = pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
     pygame.display.set_caption("Interpolation test")
     
     
-    VG.create_context((640, 480))
+    VG.create_context((width, height))
     VG.set(VG_CLEAR_COLOR, (1.0, 1.0, 1.0, 1.0))
-    paths = group_from_element(ET.parse("data/shapes.svg").getroot()[0]).children
+
+    paths = []
+    for element in ET.parse("data/shapes.svg").getroot()[0]:
+        svg_path = load_svg_element(element)
+        svg_path.path.style = svg_path.style
+        paths.append(svg_path.path)
+
     morph = VG.Path()
 
     clock = pygame.time.Clock()
@@ -38,7 +42,7 @@ def main():
                 if e.key == pygame.K_ESCAPE:
                     running = False
 
-        VG.clear((0, 0), (640, 480))
+        VG.clear((0, 0), (width, height))
         morph.clear()
 
         start = paths[i % len(paths)]
@@ -63,5 +67,4 @@ def main():
             i += 1
 
 if __name__ == '__main__':
-    main()
-    VG.destroy_context()
+    main(640, 480)
