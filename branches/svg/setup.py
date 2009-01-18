6d348bdad05b147769052f38998bbbd45f4b1477
devan.lai@gmail.com <devan.lai@gmail.com@ee642040-9953-0410-8ed3-659e4658cabf>
2009-01-18 20:09:22 +0000

    Initial checkin for svg branch

--- branches/svg/setup.py
+++ branches/svg/setup.py
@@ -10,10 +10,12 @@ FT_libraries = ["freetype"]
 
 if sys.platform == "win32":
     #Replace this with wherever your copy of the shiva source is.
-    openvg_root = r"path\to\shiva-checkout\trunk"
+    #openvg_root = r"path\to\shiva-checkout\trunk"
+    openvg_root = r"D:\powerdrop\Devan\shiva-new\trunk"
     
     #This is the default install location with the GnuWin32 installer
-    freetype_root = r"C:\GnuWin32"
+    #freetype_root = r"C:\Program Files\GnuWin32\Freetype"
+    freetype_root = r"D:\Program Files\Freetype"
 
     #if you run "build_shiva.py" then you can just use the copy
     #of libOpenVG that ends up in lib/
@@ -41,8 +43,8 @@ FT_include_dirs = ["include",
 
 #Pyrex cannot detect the change in included files
 #so it will not normally regenerate the .c
-##if os.path.exists(os.path.join("src", "OpenVG", "VG.c")):
-##    os.remove(os.path.join("src", "OpenVG", "VG.c"))
+if os.path.exists(os.path.join("src", "OpenVG", "VG.c")):
+    os.remove(os.path.join("src", "OpenVG", "VG.c"))
 
 setup(name = "PyOpenVG",
       version = "0.0.3",
@@ -56,7 +58,10 @@ setup(name = "PyOpenVG",
       package_dir = {"OpenVG":"src/OpenVG",
                      "FT":"src/FT"},
 
-      py_modules = ["OpenVG.constants", "OpenVG.font", "FT.constants"],
+      package_data = {"OpenVG":["libOpenVG.dll"]},
+
+      py_modules = ["OpenVG.constants", "OpenVG.font", "FT.constants",
+                    "OpenVG.svg", "OpenVG.svg_lxml", "OpenVG.descriptors"],
       ext_modules = [Extension("OpenVG.VG",  ["src/OpenVG/VG.pyx"],
                                libraries=VG_libraries,
                                library_dirs=VG_library_dirs,
