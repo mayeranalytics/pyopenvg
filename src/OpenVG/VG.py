8277312658d85fe05912bf8c1d51e8547459ff50
devan.lai <devan.lai@gmail.com>
2009-08-11 01:23:22 +0000

    - Replaced style_properties with the complet SVG_PROPERTY_SET and changed get_style to use the intersection of element properties and svg properties. -Rewrote the Text class to act as a pseudo-container, and added support for tspans and trefs. -Added xlink to nsmap

--- src/OpenVG/VG.py
+++ src/OpenVG/VG.py
@@ -6,6 +6,27 @@ except ImportError:
     except:
         raise RuntimeError("Unable to locate AmanithVG or ShivaVG")
 
+import constants
+
+import contextlib
+@contextlib.contextmanager
+def push(matrix, mode=None):
+    old_mode = None
+    if mode is not None:
+        old_mode = get(constants.VG_MATRIX_MODE)
+        set(VG_MATRIX_MODE, mode)
+    old_matrix = get_matrix()
+    load_matrix(matrix)
+
+    yield
+
+    if old_mode is not None:
+        set(VG_MATRIX_MODE, old_mode)
+    load_matrix(old_matrix)
+
+del contextlib
+
+
 __all__ = ["Path", "Paint", "ColorPaint", "GradientPaint", "PatternPaint",
            "Image", "Context", "Style" "VGError", "check_error", "interpolate",
            "write_image", "write_buffer", "write_to_buffer", "write_to_image",
